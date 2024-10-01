623789098bc6043b9d6f760041d8aa073875f45f
laiscrz <lais.albino2003@gmail.com>
2024-10-01 18:36:15 -0300

    feat: added comments in Repositories

--- Repositories/IRepository.cs
+++ Repositories/IRepository.cs
@@ -3,12 +3,41 @@ using System.Threading.Tasks;
 
 namespace Repositories
 {
+    /// <summary>
+    /// Interface genérica para repositórios.
+    /// </summary>
+    /// <typeparam name="T">O tipo da entidade que o repositório gerenciará.</typeparam>
     public interface IRepository<T> where T : class
     {
+        /// <summary>
+        /// Obtém todas as entidades assíncronamente.
+        /// </summary>
+        /// <returns>Uma lista de entidades do tipo <typeparamref name="T"/>.</returns>
         Task<IEnumerable<T>> GetAllAsync();
-        Task<T> GetByIdAsync(string id); 
+
+        /// <summary>
+        /// Obtém uma entidade pelo seu ID assíncronamente.
+        /// </summary>
+        /// <param name="id">O ID da entidade a ser obtida.</param>
+        /// <returns>A entidade do tipo <typeparamref name="T"/> correspondente ao ID.</returns>
+        Task<T> GetByIdAsync(string id);
+
+        /// <summary>
+        /// Adiciona uma nova entidade assíncronamente.
+        /// </summary>
+        /// <param name="entity">A entidade a ser adicionada.</param>
         Task AddAsync(T entity);
+
+        /// <summary>
+        /// Atualiza uma entidade existente assíncronamente.
+        /// </summary>
+        /// <param name="entity">A entidade com as novas informações.</param>
         Task UpdateAsync(T entity);
+
+        /// <summary>
+        /// Exclui uma entidade pelo seu ID assíncronamente.
+        /// </summary>
+        /// <param name="id">O ID da entidade a ser excluída.</param>
         Task DeleteAsync(string id);
     }
 }
--- Repositories/MovieRepository.cs
+++ Repositories/MovieRepository.cs
@@ -6,37 +6,65 @@ using Data;
 
 namespace Repositories
 {
+    /// <summary>
+    /// Implementação do repositório para gerenciar filmes.
+    /// </summary>
     public class MovieRepository : IRepository<MovieModel>
     {
         private readonly IMongoCollection<MovieModel> _movies;
 
+        /// <summary>
+        /// Construtor que inicializa uma nova instância da classe <see cref="MovieRepository"/>.
+        /// </summary>
+        /// <param name="mongoDbFactory">Fábrica para criar a conexão com o MongoDB.</param>
+        /// <param name="mongoDbSettings">Configurações do MongoDB.</param>
         public MovieRepository(MongoDbFactory mongoDbFactory, MongoDbSettings mongoDbSettings)
         {
             var database = mongoDbFactory.GetDatabase();
             _movies = database.GetCollection<MovieModel>(mongoDbSettings.CollectionName);
         }
 
-
+        /// <summary>
+        /// Obtém todos os filmes assíncronamente.
+        /// </summary>
+        /// <returns>Uma lista de <see cref="MovieModel"/>.</returns>
         public async Task<IEnumerable<MovieModel>> GetAllAsync()
         {
             return await _movies.Find(movie => true).ToListAsync();
         }
 
+        /// <summary>
+        /// Obtém um filme pelo seu ID assíncronamente.
+        /// </summary>
+        /// <param name="id">O ID do filme a ser obtido.</param>
+        /// <returns>O filme correspondente ao ID.</returns>
         public async Task<MovieModel> GetByIdAsync(string id)
         {
             return await _movies.Find(Builders<MovieModel>.Filter.Eq(m => m.Id, id)).FirstOrDefaultAsync();
         }
 
+        /// <summary>
+        /// Adiciona um novo filme assíncronamente.
+        /// </summary>
+        /// <param name="movie">O filme a ser adicionado.</param>
         public async Task AddAsync(MovieModel movie)
         {
             await _movies.InsertOneAsync(movie);
         }
 
+        /// <summary>
+        /// Atualiza um filme existente assíncronamente.
+        /// </summary>
+        /// <param name="movie">O filme com as novas informações.</param>
         public async Task UpdateAsync(MovieModel movie)
         {
             await _movies.ReplaceOneAsync(Builders<MovieModel>.Filter.Eq(m => m.Id, movie.Id), movie);
         }
 
+        /// <summary>
+        /// Exclui um filme pelo seu ID assíncronamente.
+        /// </summary>
+        /// <param name="id">O ID do filme a ser excluído.</param>
         public async Task DeleteAsync(string id)
         {
             await _movies.DeleteOneAsync(Builders<MovieModel>.Filter.Eq(m => m.Id, id));
