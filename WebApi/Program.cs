4a7f40854ed7fbdeae12fcf01b4938a63e28807d
laiscrz <lais.albino2003@gmail.com>
2024-10-01 15:37:23 -0300

    feat: update Program.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,21 +1,32 @@
-using Data; 
+using Data;
 using Microsoft.Extensions.Options;
+using Models;
+using Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<MongoDbSettings>(
     builder.Configuration.GetSection("MongoDbSettings"));
 
-builder.Services.AddSingleton<IMongoDbSettings>(sp =>
+
+builder.Services.AddSingleton(sp =>
     sp.GetRequiredService<IOptions<MongoDbSettings>>().Value);
 
-builder.Services.AddSingleton<MongoDbFactory>(); 
+
+builder.Services.AddSingleton<MongoDbFactory>();
+
+builder.Services.AddScoped<IRepository<MovieModel>>(sp =>
+{
+    var mongoDbFactory = sp.GetRequiredService<MongoDbFactory>();
+    var mongoDbSettings = sp.GetRequiredService<MongoDbSettings>();
+    return new MovieRepository(mongoDbFactory, mongoDbSettings);
+});
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configuração do pipeline HTTP
+
 app.UseAuthorization();
 app.MapControllers();
 
