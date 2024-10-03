963cef5fa9eb94cf55f9549a45b1b9c5aa5aec0a
laiscrz <lais.albino2003@gmail.com>
2024-10-03 19:38:24 -0300

    feat: Implemented unit testing and integration

---
+++ Tests/Integration/MovieRepositoryTests.cs
@@ -0,0 +1,53 @@
+using Xunit;
+using MongoDB.Driver;
+using Data;
+using Models;
+using Repositories;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationTests
+{
+    public class MovieRepositoryTests
+    {
+        private MovieRepository _movieRepository;
+        private MongoDbFactory _mongoDbFactory;
+
+        public MovieRepositoryTests()
+        {
+            var mongoDbSettings = new MongoDbSettings
+            {
+                ConnectionString = "mongodb+srv://lais_db:mongo552258@clusterdatabase.mn5ft.mongodb.net/?retryWrites=true&w=majority&appName=ClusterDatabase",
+                DatabaseName = "TestMovixBD",
+                CollectionName = "movies"
+            };
+
+            _mongoDbFactory = new MongoDbFactory(mongoDbSettings);
+            _movieRepository = new MovieRepository(_mongoDbFactory, mongoDbSettings);
+        }
+
+        [Fact]
+        public async Task CreateMovie_ShouldInsertMovie()
+        {
+            // Arrange
+            var movie = new MovieModel
+            {
+                Titulo = "Teste de Integração",
+                Diretor = "Diretor Exemplo",
+                AnoLancamento = 2024,
+                Genero = new List<string> { "Ação", "Aventura" },
+                Sinopse = "Filme de teste de integração."
+            };
+
+            // Act
+            await _movieRepository.CreateAsync(movie);
+
+            // Assert
+            var insertedMovie = await _movieRepository.GetByIdAsync(movie.Id.ToString());
+            Assert.NotNull(insertedMovie);
+            Assert.Equal(movie.Titulo, insertedMovie.Titulo);
+        }
+
+    }
+}
